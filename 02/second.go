4a7c9ac817f2841e6509e88cb8c211090ef7d3a1
mightypanders <markus.dieckmann@posteo.de>
2021-12-03 18:04:34 +0100

    refactor second day

--- 02/second.go
+++ 02/second.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 	"strconv"
@@ -12,42 +11,33 @@ import (
 var vertical int = 0
 var horizontal int = 0
 var aim int = 0
+
 func checkErr(e error){
 	if e != nil{
-		log.Fatal(e)
 		panic(e)
 	}
 }
 func main(){
-	f, err:=os.Open("input")
-	checkErr(err)
+	f,e:=os.Open("input")
+	checkErr(e)
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan(){
 		evalCommand(scanner.Text())
 	}
-	fmt.Println(horizontal*vertical)
+	defer fmt.Println(horizontal*vertical)
 }
 func evalCommand(c string){
 	s := strings.Split(c," ")
-
+	value,e:=	strconv.Atoi(s[1])
+	checkErr(e)
 	switch s[0]{
 	case "forward":
-		s,err :=	strconv.Atoi(s[1])
-		checkErr(err)
-		horizontal = horizontal+s
-		vertical = vertical+(aim*s)
-	case "backward":
-		s,err :=	strconv.Atoi(s[1])
-		checkErr(err)
-		horizontal = horizontal-s
+		horizontal += value
+		vertical += aim* value
 	case "up":
-		s,err :=	strconv.Atoi(s[1])
-		checkErr(err)
-		aim = aim-s
+		aim -= value
 	case "down":
-		s,err :=	strconv.Atoi(s[1])
-		checkErr(err)
-		aim = aim+s
+		aim += value
 	}
 }
